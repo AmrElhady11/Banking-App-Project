513ae0bb1129331831f097a628236f905e3e2ffc
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-03-30 23:27:43 +0200

    creating AccountsRepository interface

---
+++ accounts-service/src/main/java/com/accounts/repository/AccountsRepository.java
@@ -0,0 +1,11 @@
+package com.accounts.repository;
+
+import com.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AccountsRepository extends JpaRepository<Accounts,Long> {
+    Optional<Accounts> findByPublicAccountNumber(UUID publicAccountNumber);
+}
