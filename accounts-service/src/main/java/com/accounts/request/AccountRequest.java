40281b85f92759e574d1be81c22ea2523bdf1033
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-03-30 23:30:05 +0200

    creating AccountRequest and adding its validation

---
+++ accounts-service/src/main/java/com/accounts/request/AccountRequest.java
@@ -0,0 +1,25 @@
+package com.accounts.request;
+
+import com.accounts.dto.CustomerDto;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountRequest {
+    @NotEmpty(message = "Account number cannot be null or empty")
+    private UUID publicAccountNumber;
+    @NotEmpty(message = "AccountType cannot be null or empty")
+    private String accountType;
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
+    private String branchAddress;
+    private CustomerDto customer;
+}
