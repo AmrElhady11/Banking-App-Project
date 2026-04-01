931c81e23af919702168fa703c1a8ebe87ccd58e
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-01 02:44:12 +0200

    refactoring IAccountService by editing the pramaters of deleteAccountMethod

--- accounts-service/src/main/java/com/accounts/service/IAccountService.java
+++ accounts-service/src/main/java/com/accounts/service/IAccountService.java
@@ -4,10 +4,11 @@ import com.accounts.dto.CustomerDto;
 import com.accounts.request.AccountRequest;
 
 import javax.security.auth.login.AccountNotFoundException;
+import java.util.UUID;
 
 public interface IAccountService {
     void createAccount(AccountRequest accountRequest) throws AccountNotFoundException;
     CustomerDto fetchAccount(String mobileNumber) throws AccountNotFoundException;
     boolean updateAccount(AccountRequest accountRequest) throws AccountNotFoundException;
-    boolean deleteAccount(String mobileNumber);
+    boolean deleteAccount(UUID accountNumber) throws AccountNotFoundException;
 }
