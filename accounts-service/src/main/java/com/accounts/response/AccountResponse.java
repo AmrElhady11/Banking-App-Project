174a2624c1bdc5c0179c10ad4db649e234013288
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-01 04:19:58 +0200

    creating AccountResponse class

---
+++ accounts-service/src/main/java/com/accounts/response/AccountResponse.java
@@ -0,0 +1,16 @@
+package com.accounts.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountResponse {
+    private String statusMessage;
+    private HttpStatus statusCode;
+}
