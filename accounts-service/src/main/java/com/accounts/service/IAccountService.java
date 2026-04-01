58cc877684fa2c2a0caa8a2a9e42b13bf5ae16f7
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-01 04:18:47 +0200

    refactoring the methods in the interface to throws the custom exceptions rather than built-in exceptions

--- accounts-service/src/main/java/com/accounts/service/IAccountService.java
+++ accounts-service/src/main/java/com/accounts/service/IAccountService.java
@@ -1,14 +1,15 @@
 package com.accounts.service;
 
 import com.accounts.dto.CustomerDto;
+import com.accounts.exception.ResourceNotFoundException;
 import com.accounts.request.AccountRequest;
 
 import javax.security.auth.login.AccountNotFoundException;
 import java.util.UUID;
 
 public interface IAccountService {
-    void createAccount(AccountRequest accountRequest) throws AccountNotFoundException;
-    CustomerDto fetchAccount(String mobileNumber) throws AccountNotFoundException;
-    boolean updateAccount(AccountRequest accountRequest) throws AccountNotFoundException;
-    boolean deleteAccount(UUID accountNumber) throws AccountNotFoundException;
+    void createAccount(AccountRequest accountRequest) throws ResourceNotFoundException;
+    CustomerDto fetchAccount(String mobileNumber) throws ResourceNotFoundException;
+    boolean updateAccount(AccountRequest accountRequest) throws ResourceNotFoundException;
+    boolean deleteAccount(UUID accountNumber) throws ResourceNotFoundException;
 }
