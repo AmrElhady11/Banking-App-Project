4ed9e70fd19039fe572d6a5617800890c36a4eda
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-01 04:19:35 +0200

    refactoring AccountServiceImpl's methods to throw custom exceptions

--- accounts-service/src/main/java/com/accounts/service/impl/AccountServiceImpl.java
+++ accounts-service/src/main/java/com/accounts/service/impl/AccountServiceImpl.java
@@ -3,6 +3,9 @@ package com.accounts.service.impl;
 import com.accounts.dto.CustomerDto;
 import com.accounts.entity.Accounts;
 import com.accounts.entity.Customer;
+import com.accounts.exception.ResourceNotFoundException;
+import com.accounts.mapper.AccountsMapper;
+import com.accounts.mapper.CustomerMapper;
 import com.accounts.repository.AccountsRepository;
 import com.accounts.repository.CustomerRepository;
 import com.accounts.request.AccountRequest;
@@ -11,7 +14,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.util.UUID;
 
 @Service
@@ -19,50 +21,41 @@ import java.util.UUID;
 public class AccountServiceImpl implements IAccountService {
     private final CustomerRepository customerRepository;
     private final AccountsRepository accountsRepository;
+    private final AccountsMapper accountsMapper;
+    private final CustomerMapper customerMapper;
+
     @Transactional
     @Override
-    public void createAccount(AccountRequest accountRequest) throws AccountNotFoundException {
-        Customer theCustomer = getCustomerByEmail(accountRequest.getCustomer().getEmail());
-        Accounts theAccount = Accounts.builder()
-                .accountType(accountRequest.getAccountType())
-                .branchAddress(accountRequest.getBranchAddress())
-                .customerId(theCustomer.getCustomerId())
-                .build();
+    public void createAccount(AccountRequest accountRequest) throws ResourceNotFoundException {
+        getCustomerByEmail(accountRequest.getCustomer().getEmail());
+        Accounts theAccount = accountsMapper.toAccounts(accountRequest);
         accountsRepository.save(theAccount);
     }
 
 
 
     @Override
-    public CustomerDto fetchAccount(String mobileNumber) throws AccountNotFoundException {
+    public CustomerDto fetchAccount(String mobileNumber) throws ResourceNotFoundException {
         Customer theCustomer = getCustomerByMobileNumber(mobileNumber);
 
-        return CustomerDto.builder()
-                .email(theCustomer.getEmail())
-                .mobileNumber(theCustomer.getMobileNumber())
-                .name(theCustomer.getName())
-                .build();
+        return customerMapper.toCustomerDto(theCustomer);
     }
 
     @Override
     @Transactional
-    public boolean updateAccount(AccountRequest accountRequest) throws AccountNotFoundException {
+    public boolean updateAccount(AccountRequest accountRequest) throws ResourceNotFoundException {
         Accounts theAccount = getAccountByAccountNumber(accountRequest.getAccountNumber());
         Customer theCustomer = getCustomerByEmail(accountRequest.getCustomer().getEmail());
         if(theAccount==null)
             return false;
-        theAccount = Accounts.builder()
-                .accountType(accountRequest.getAccountType())
-                .branchAddress(accountRequest.getBranchAddress())
-                .customerId(theCustomer.getCustomerId())
-                .build();
+        theAccount = accountsMapper.toAccounts(accountRequest);
         accountsRepository.save(theAccount);
         return true;
     }
 
     @Override
     @Transactional
-    public boolean deleteAccount(UUID accountNumber) throws AccountNotFoundException {
+    public boolean deleteAccount(UUID accountNumber) throws ResourceNotFoundException {
         Accounts theAccount = getAccountByAccountNumber(accountNumber);
         if(theAccount==null)
             return false;
@@ -71,14 +64,14 @@ public class AccountServiceImpl implements IAccountService {
     }
 
 
-    private Customer getCustomerByEmail(String email) throws AccountNotFoundException {
-        return customerRepository.findByEmail(email).orElseThrow(()-> new AccountNotFoundException(email));
+    private Customer getCustomerByEmail(String email) throws ResourceNotFoundException {
+        return customerRepository.findByEmail(email).orElseThrow(()-> new ResourceNotFoundException("Customer","email",email));
     }
-    private Customer getCustomerByMobileNumber(String mobileNumber) throws AccountNotFoundException {
-        return customerRepository.findByMobileNumber(mobileNumber).orElseThrow(()-> new AccountNotFoundException(mobileNumber));
+    private Customer getCustomerByMobileNumber(String mobileNumber) throws ResourceNotFoundException {
+        return customerRepository.findByMobileNumber(mobileNumber).orElseThrow(()-> new ResourceNotFoundException("Customer","mobileNumber",mobileNumber));
     }
-    private Accounts getAccountByAccountNumber(UUID accountNumber) throws AccountNotFoundException {
-        return accountsRepository.findByAccountNumber(accountNumber).orElseThrow(AccountNotFoundException::new);
+    private Accounts getAccountByAccountNumber(UUID accountNumber) throws ResourceNotFoundException{
+        return accountsRepository.findByAccountNumber(accountNumber).orElseThrow(()-> new ResourceNotFoundException("Account","accountNumber",accountNumber.toString()));
 
     }
 }
