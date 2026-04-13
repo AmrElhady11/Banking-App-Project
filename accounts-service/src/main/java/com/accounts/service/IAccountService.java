d5c7bb3692e10f7b316cf4f40fd74aabc72a807a
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-13 14:17:06 +0200

    refactoring uuid to integer

--- accounts-service/src/main/java/com/accounts/service/IAccountService.java
+++ accounts-service/src/main/java/com/accounts/service/IAccountService.java
@@ -3,13 +3,11 @@ package com.accounts.service;
 import com.accounts.dto.CustomerDto;
 import com.accounts.exception.ResourceNotFoundException;
 import com.accounts.request.AccountRequest;
-
-import javax.security.auth.login.AccountNotFoundException;
 import java.util.UUID;
 
 public interface IAccountService {
     void createAccount(AccountRequest accountRequest) throws ResourceNotFoundException;
     CustomerDto fetchAccount(String mobileNumber) throws ResourceNotFoundException;
     boolean updateAccount(AccountRequest accountRequest) throws ResourceNotFoundException;
-    boolean deleteAccount(UUID accountNumber) throws ResourceNotFoundException;
+    boolean deleteAccount(Integer accountNumber) throws ResourceNotFoundException;
 }
