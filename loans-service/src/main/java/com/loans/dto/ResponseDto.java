7940bda7293ba5468dc4ae17185abe95fea276c6
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-13 22:42:05 +0200

    create ResponseDto

---
+++ loans-service/src/main/java/com/loans/dto/ResponseDto.java
@@ -0,0 +1,16 @@
+package com.loans.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDto {
+    private HttpStatus statusCode;
+    private String message;
+}
