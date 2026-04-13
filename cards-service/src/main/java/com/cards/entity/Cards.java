aad9e7cb029299106250575ed1e7141b94b11847
AmrElhady11 <amr.elhady.programs@gmail.com>
2026-04-13 17:39:03 +0200

    creating Cards Entity class

---
+++ cards-service/src/main/java/com/cards/entity/Cards.java
@@ -0,0 +1,24 @@
+package com.cards.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cards")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Cards {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cardId;
+    private String mobileNumber;
+    private String cardNumber;
+    private String cardType;
+    private int totalLimit;
+    private int amountUsed;
+    private int availableAmount;
+}
